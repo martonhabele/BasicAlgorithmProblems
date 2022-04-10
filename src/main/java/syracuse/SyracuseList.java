58693e6365d75d55ba47714256e884d04775b21c
Márton András Hábele <marton.habele@gmail.com>
2022-04-10 19:28:23 +0200

    Unwanted add method removed, comment added to explain what is this code do.

--- src/main/java/syracuse/SyracuseList.java
+++ src/main/java/syracuse/SyracuseList.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// Basic & inefficient algorithm for generating numbers for the Collatz conjecture
+// Every number you give generates a list ending with the recursive numbers of [4, 2, 1]
+
 public class SyracuseList {
     public static void main(String[] args) {
         List<Long> result = new ArrayList<>();
@@ -11,21 +14,20 @@ public class SyracuseList {
         boolean isLoop = false;
 
         System.out.print("Give me a number: ");
-        long inputNumber = sc.nextLong();             //No exception handling; reused variable
+        long inputNumber = sc.nextLong();             // No exception handling; reused variable
         result.add(inputNumber);
 
-        while (isLoop == false) {
-            if (inputNumber == 1) {
+        while (!isLoop) {
+            if (inputNumber == 1) {                   // Separate if-clause for searching the value 1
                 isLoop = true;
-                result.add(inputNumber);
                 System.out.println(result);
             }
 
-            if (inputNumber % 2 == 0) {
+            if (inputNumber % 2 == 0) {               // If even, then divide by 2
                 inputNumber /= 2;
                 result.add(inputNumber);
             } else {
-                inputNumber = 3 * inputNumber + 1;
+                inputNumber = 3 * inputNumber + 1;    // If odd, multiply by 3 and add 1
                 result.add(inputNumber);
             }
         }
